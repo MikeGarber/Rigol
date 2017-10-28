1dd117dfa23e0aacad85c1517ddd2257f4e59a2b
MikeGarber <mgarber@wi.rr.com>
2017-10-27 22:35:07 -0500

    Just 2 buttons.

--- DAckMeasure/UI_Rigol_1.py
+++ DAckMeasure/UI_Rigol_1.py
@@ -17,12 +17,14 @@ class MainApplication(tk.Frame):
         self.chans = []
         for i in range(4):
             self.chans.append(tk.IntVar())
-        self.startButton = tk.Button(self, text="Start/Restart", command=self.onStartButton)
-        self.startButton.grid(row=0, column=0)
-        self.pauseResumeButton = tk.Button(self, text="Pause", state=tk.DISABLED, command=self.onPauseResumeButton)
-        self.pauseResumeButton.grid(row=1, column=0)
+        #self.startButton = tk.Button(self, text="Start/Restart", command=self.onStartButton)
+        #self.startButton.grid(row=0, column=0)
+        #self.pauseResumeButton = tk.Button(self, text="Pause", state=tk.DISABLED, command=self.onPauseResumeButton)
+        #self.pauseResumeButton.grid(row=1, column=0)
+        self.StartStopButton = tk.Button(self, text="Start", command=self.onStartStopButton)
+        self.StartStopButton.grid(row=0, column=0)
         self.showDataButton = tk.Button(self, text="Show Data", command=self.onShowDataButton)
-        self.showDataButton.grid(row=2, column=0)
+        self.showDataButton.grid(row=1, column=0)
         self.Chan1Button = tk.Checkbutton(self, text="Chan 1", variable=self.chans[0])
         self.Chan1Button.grid(row=0, column=1)
         self.Chan2Button = tk.Checkbutton(self, text="Chan 2", variable=self.chans[1])
@@ -35,7 +37,7 @@ class MainApplication(tk.Frame):
         self.timeText.grid(row=3, column=0)
 
         vcmd = (self.register(self.validate),'%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
-        self.timebaseBox = tk.Entry(self, validate = 'key', validatecommand = vcmd, justify='right', text="0")
+        self.timebaseBox = tk.Entry(self, validate = 'key', validatecommand = vcmd, justify='right', text="1")
         self.timebaseBox.grid(row=4, column=0)
         self.timebaseBox.focus()
         self.timeText = tk.Label(self, text="Sec", justify='left')
@@ -57,13 +59,6 @@ class MainApplication(tk.Frame):
         else:
             return True
     
-    def onStartButton(self):
-        self.running=True
-        self.elapsed=0
-        self.startButton.config(state=tk.DISABLED)
-        self.pauseResumeButton.config(state=tk.NORMAL, text="Pause")
-        self.timebase=int(self.timebaseBox.get())*1000
-
     def onPauseResumeButton(self):
         PauseButTxt="Pause"
         runningState=True
@@ -75,13 +70,20 @@ class MainApplication(tk.Frame):
         self.pauseResumeButton.config(text=PauseButTxt)
         self.startButton.config(state=startButEnab)
         self.running=runningState
-
+ 
+    def onStartStopButton(self):
+        self.StartStopButton.config(text=("Stop", "Start")[self.running])
+        self.running= not self.running
+        if (self.running):
+            self.timebase=int(self.timebaseBox.get())*1000
+            self.elapsed=0
+            
     def onShowDataButton(self):
         pass
 
     def onTimeUpdate(self):
         if (self.running):
-            self.elapsed += self.timebase
+            self.elapsed += self.timebase/1000
             self.timeText.config(text=self.elapsed)
         self.after(self.timebase, self.onTimeUpdate)
 
